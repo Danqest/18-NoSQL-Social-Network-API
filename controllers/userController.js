27a744a500f3c6ed60c599de4b7ad6f514401c92
CBrooks <mcbrooks@pm.me>
2022-07-11 15:53:12 -0500

    finalized user controller and user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const { User } = require('../models');
 
 module.exports = {
+  // Get all users
   getUsers(req, res) {
     User.find()
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  // Get a single user
+  // Get a single user by ID
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .then((user) =>
@@ -20,6 +21,39 @@ module.exports = {
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a user by ID
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+      )
+      .then((user) =>
+      !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : res.json(
+          [
+            { 
+              message: 'User successfully updated'
+            },
+            {
+              user
+            }
+          ]
+        )
+    )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a user by ID
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({ message: 'User successfully deleted'})
+      )
       .catch((err) => res.status(500).json(err));
   },
 };
